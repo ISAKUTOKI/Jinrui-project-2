dad3fd9f9372c3c9e8c6f1d977af933f3e7fbb32
ISAKUTOKI <isakutoki@gmail.com>
2024-12-08 15:13:31 +0800

    2024/12/08 images update

---
+++ 2dPlatform/Assets/Game/Script/test/ScreenshotTaker.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class ScreenshotTaker : MonoBehaviour
+{
+    public int i = 1;
+
+    private void Update()
+    {
+        // 检查是否按下了F10键
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            // 调用截图方法
+            TakeScreenshot();
+        }
+    }
+
+    private void TakeScreenshot()
+    {
+
+        // 保存截图到磁盘
+        string path = System.IO.Path.Combine(Application.persistentDataPath, "Screenshot" + (i++) + ".png");
+        ScreenCapture.CaptureScreenshot(path);
+        Debug.Log("Screenshot saved: " + path);
+    }
+}
