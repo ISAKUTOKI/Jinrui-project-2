91cd4123958739680d84ae0a98a594612e96183b
Aieylisyh <Aieylisyh@users.noreply.github.com>
2024-12-08 16:28:08 +0800

    wip

---
+++ 2dPlatform/Assets/Game/Script/HUD_interface/Combat/WeaponPowerSystem.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Script.HUD_interface.Combat
+{
+    public class WeaponPowerSystem : MonoBehaviour
+    {
+        public static WeaponPowerSystem instance;
+
+        public WeaponUiBehaviour weaponUiBehaviour;
+
+        public float power { get; private set; }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        /// <summary>
+        /// 当玩家使出攻击时，如果有能量，就消耗能量并使出更强的攻击
+        /// </summary>
+        /// <returns>玩家的这一击是否消耗能量</returns>
+        public bool OnPlayerAttackPerformed(bool consumeThePower = true)
+        {
+            if (power >= 0.1f)
+            {
+                if (consumeThePower)
+                {
+                    ConsumePower_cell(1);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool OnPlayerDefendPerformed(bool consumeThePower = true)
+        {
+            if (power >= 0.1f)
+            {
+                if (consumeThePower)
+                {
+                    ConsumePower_cell(1);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool OnPlayerDefending(float v)
+        {
+            if (power >= v)
+            {
+                ConsumePower_smoothly(v);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ConsumePower_smoothly(float v)
+        {
+            power -= v;
+            weaponUiBehaviour.SyncPowerValue(power);
+        }
+
+        public void ConsumePower_cell(int c)
+        {
+            if (power == 3)
+            {
+                power -= 1;//2
+            }
+            else if (power > 2 && power < 3)
+            {
+                power = 2;
+            }
+            else if (power == 2)
+            {
+                power = 1;
+            }
+            else if (power > 1 && power < 2)
+            {
+                power = 1;
+            }
+            else if (power == 1)
+            {
+                power = 0;
+            }
+            else if (power > 0.1f && power < 1)
+            {
+                power = 0;
+            }
+
+            weaponUiBehaviour.SyncPowerValue(power);
+        }
+
+        public void GainOnePower()
+        {
+            if (power < 1)
+            {
+                power = 1;
+                weaponUiBehaviour.PlayP0_P1Anim();
+            }
+            else if (power < 2)
+            {
+                power = 2;
+                weaponUiBehaviour.PlayP1_P2Anim();
+            }
+            else
+            {
+                power = 3;
+                weaponUiBehaviour.PlayP2_P3Anim();
+            }
+
+            weaponUiBehaviour.SyncPowerValue(power);
+        }
+
+        public void GainFullPower()
+        {
+            power = 3;
+            weaponUiBehaviour.PlayPMaxAnim();
+            weaponUiBehaviour.SyncPowerValue(power);
+        }
+
+        public void 测试_开始防御()
+        {
+            _test_defending = true;
+        }
+
+        public void 测试_结束防御()
+        {
+            _test_defending = false;
+        }
+
+        bool _test_defending;
+
+        private void Update()
+        {
+            if (_test_defending)
+            {
+                float v = Time.deltaTime * 0.5f;
+                var res = OnPlayerDefending(v);
+                if (!res)
+                {
+                    _test_defending = false;
+                    Debug.Log("能量耗尽，自动结束防御");
+                }
+            }
+        }
+
+        public void 测试_格挡或攻击触发()
+        {
+            OnPlayerAttackPerformed(true);
+            //OnPlayerDefendPerformed
+        }
+
+        public void 测试_弹反触发()
+        {
+            GainOnePower();
+        }
+
+        public void 测试_超级弹反触发()
+        {
+            GainFullPower();
+        }
+    }
+}
---
+++ 2dPlatform/Assets/Game/Script/HUD_interface/Combat/WeaponUiBehaviour.cs
@@ -0,0 +1,121 @@
+﻿using Assets.Game.Script.HUD_interface.Combat;
+using com;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUiBehaviour : MonoBehaviour
+{
+    // empty p1 p2 p3
+    public UiImageAnimation uia;
+
+    public WeaponUiPart p1;
+    public WeaponUiPart p2;
+    public WeaponUiPart p3;
+
+    public class WeaponUiPart
+    {
+        public Image img;
+        public void SetEmpty()
+        {
+            img.fillAmount = 0;
+        }
+
+        public void SetFull()
+        {
+            img.fillAmount = 1;
+        }
+
+        public void SetPercent(float p)
+        {
+            float a = 0.04f + p * 0.91f;
+            img.fillAmount = a;
+        }
+    }
+
+    public void SyncPowerValue(float v)
+    {
+        if (v == 3)
+        {
+            p1.SetFull();
+            p2.SetFull();
+            p3.SetFull();
+        }
+        else if (v > 2 && v < 3)
+        {
+            p1.SetFull();
+            p2.SetFull();
+            p3.SetPercent(v - 2);
+        }
+        else if (v == 2)
+        {
+            p1.SetFull();
+            p2.SetFull();
+            p3.SetEmpty();
+        }
+        else if (v > 1 && v < 2)
+        {
+            p1.SetFull();
+            p2.SetPercent(v - 1);
+            p3.SetEmpty();
+        }
+        else if (v == 1)
+        {
+            p1.SetFull();
+            p2.SetEmpty();
+            p3.SetEmpty();
+        }
+        else if (v > 0 && v < 1)
+        {
+            p1.SetPercent(v - 0);
+            p2.SetEmpty();
+            p3.SetEmpty();
+        }
+        else if (v == 0)
+        {
+            p1.SetEmpty();
+            p2.SetEmpty();
+            p3.SetEmpty();
+        }
+    }
+
+    /// <summary>
+    ///  use power : attack consume 1 cell
+    ///  non full cell: when use 1 cell, use the non interger part
+    /// </summary>
+    /// <param name="p"></param>
+
+    public void TogglePowerFullAnim(bool playOrStop)
+    {
+        if (playOrStop)
+            uia.Play(4);
+        else
+            uia.Stop();
+    }
+
+    public void PlayP0_P1Anim()
+    {
+        uia.Play(1);
+    }
+
+    public void PlayP1_P2Anim()
+    {
+        uia.Play(2);
+    }
+
+    public void PlayP2_P3Anim()
+    {
+        uia.Play(3);
+    }
+
+    public void PlayPMaxAnim()
+    {
+        uia.Play(0);
+        uia.SetPlayEndCallback(
+            () =>
+            {
+                TogglePowerFullAnim(true);
+            }
+            );
+    }
+}
