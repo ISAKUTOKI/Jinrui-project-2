086b43bbec444fcbddfd7486d3578a11f3342b65
ISAKUTOKI <isakutoki@gmail.com>
2024-12-15 16:53:56 +0800

    WIP

--- 2dPlatform/Assets/PlayerBougyo.cs
+++ 2dPlatform/Assets/PlayerBougyo.cs
@@ -14,35 +14,52 @@ public class PlayerBougyo : MonoBehaviour
 
     void Start()
     {
-
+        Debug.Log("canDebugLog");
+        Debug.Log(WPS.power);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (WPS.power >= canBougyoValue)
-            canBougyo = true;
-        else
-            canBougyo = false;
+        if (Input.GetKeyDown(KeyCode.F12))
+        Debug.Log(WPS.power);
 
-
-        if (Input.GetKeyDown(KeyCode.K) && canBougyo)
+        if (WPS.power > canBougyoValue)
         {
+            canBougyo = true;
+            //Debug.Log("canbougyo");
+        }
+        else
+        {
+            canBougyo = false;
+            //Debug.Log("canNotBougyo");
+        }
+
+
+
+        //按下K并且可以防御并且不在防御那就进入防御
+        if (Input.GetKeyDown(KeyCode.K) && canBougyo && inBougyo == false)
+        {
+            //防御动画
             PlayerBehaviour.instance.animator.SetTrigger("bougyo_start");
-            Debug.Log("bougyo start");
+            //debug开始防御
+            //Debug.Log("bougyo start");
+            //防御中
             inBougyo = true;
         }
 
-        if (inBougyo && canBougyo == false || Input.GetKeyDown(KeyCode.K) == false)
+        //在防御中
+        if (inBougyo == true && Input.GetKeyDown(KeyCode.K) == false)
         {
             PlayerBehaviour.instance.animator.SetTrigger("bougyo_out");
-            Debug.Log("bougyo out");
+            //Debug.Log("检测");
         }
-
-
 
     }
 
+    public void BougyoEnd()
+    {
 
+    }
 
 }
