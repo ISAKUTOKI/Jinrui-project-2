ff8fae88a56a7edf65bbeb5ba7449f50d42d3b2c
ISAKUTOKI <isakutoki@gmail.com>
2025-01-10 23:40:25 +0800

    WIP
    石中剑的动画完成，提示按钮的动画完成，摄像机的缩放完成了一部分，还有bug需要修

---
+++ 2dPlatform/Assets/Game/Script/SceneObject/SwordInStone/SIS_JKey.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SIS_JKey : MonoBehaviour
+{
+    public GameObject JKey;
+    public SpriteRenderer JKeyRenderer;
+
+    [HideInInspector] public bool seeable = false;
+    [HideInInspector] public bool canSelfRotate = false;
+    [HideInInspector] public bool canSelfScale = false;
+    [HideInInspector] public bool isTappingKey = false;
+    [HideInInspector] public bool isTapScaling = false;
+
+    //以下为旋转用的变量
+    private float rotationSpeed = 3.0f; // 旋转速度
+    private float maxRotationAngle = 2.0f; // 最大旋转角度
+    private bool rotateClockwise = true; // 旋转方向，true为顺时针，false为逆时针
+    private float currentRotationAngle = 0.0f; // 当前旋转角度
+
+    //以下为缩放用的变量
+    private float scaleSpeed = 1.8f; // 缩放速度
+    private float maxScale = 1.3f; // 最大缩放比例
+    private float minScale = 1.0f; // 最小缩放比例
+    private bool isScalingUp = true; // 缩放方向，true为放大，false为缩小
+    private Vector3 originalScale; // 物体的原始缩放比例
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HintKeyVisibility();
+        if (canSelfRotate)
+            SelfRotate();
+        if (canSelfScale)
+            SelfScale();
+        if (isTappingKey)
+            TappingKey();
+    }
+
+
+    void HintKeyVisibility()
+    {
+        if (seeable)
+            JKeyRenderer.enabled = true;
+        if (!seeable)
+            JKeyRenderer.enabled = false;
+    }
+    void SelfRotate()
+    {
+        if (rotateClockwise)
+        {
+            currentRotationAngle += rotationSpeed * Time.deltaTime;
+            if (currentRotationAngle >= maxRotationAngle)
+            {
+                rotateClockwise = false; // 达到最大角度，改变旋转方向
+            }
+        }
+        else
+        {
+            currentRotationAngle -= rotationSpeed * Time.deltaTime;
+            if (currentRotationAngle <= -maxRotationAngle)
+            {
+                rotateClockwise = true; // 达到最小角度，改变旋转方向
+            }
+        }
+        // 应用旋转到物体
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRotationAngle);
+    }
+    void SelfScale()
+    {
+        // 根据缩放方向更新当前缩放比例
+        if (isScalingUp)
+        {
+            // 逐步放大
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale * maxScale, scaleSpeed * Time.deltaTime);
+            // 检查是否达到最大缩放比例
+            if (transform.localScale.x >= originalScale.x * maxScale - 0.1f)
+            {
+                isScalingUp = false; // 改变缩放方向为缩小
+            }
+        }
+        else
+        {
+            // 逐步缩小
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale * minScale, scaleSpeed * Time.deltaTime);
+            // 检查是否达到最小缩放比例
+            if (transform.localScale.x <= originalScale.x * minScale + 0.1f)
+            {
+                isScalingUp = true; // 改变缩放方向为放大
+            }
+        }
+    }
+
+    void TappingKey()
+    {
+        if (isTappingKey && Input.GetKeyDown(KeyCode.J) && !isTapScaling) // 检查是否按下J键并且当前没有缩放操作在进行
+        {
+            StartCoroutine(ScaleJKey());
+            Debug.Log("OK");
+        }
+
+        if (isTapScaling)
+            Debug.Log("is Scaling");
+    }
+    IEnumerator ScaleJKey()
+    {
+        isTapScaling = true; // 开始缩放操作，设置标志为true
+
+        yield return StartCoroutine(ScaleOverTime(1.3f, 0.1f));
+
+        yield return StartCoroutine(ScaleOverTime(0.7f, 0.1f));
+
+        isTapScaling = false; // 缩放操作完成，设置标志为false
+    }//控制缩放的协程
+    IEnumerator ScaleOverTime(float targetScale, float duration)
+    {
+        float startTime = Time.time;
+        Vector3 originalScale = transform.localScale;
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            transform.localScale = Vector3.Lerp(originalScale, originalScale * targetScale, t);
+            yield return null;
+        }
+
+        transform.localScale = originalScale * targetScale;
+    }//进行缩放动作的协程
+}
--- 2dPlatform/Assets/Game/Script/SceneObject/SwordInStone/SwordInStoneBehavior.cs
+++ 2dPlatform/Assets/Game/Script/SceneObject/SwordInStone/SwordInStoneBehavior.cs
@@ -1,129 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SwordInStoneBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator animator;
-    public Object Neko;
+    public GameObject Neko;
+    public Camera Camera;
+    public SIS_JKey JKey;
     bool canTryToGetSword = false;
 
-    //这下面都是用于检测拔剑的变量
+    //以下为用于检测拔剑的变量
     int tapCounter = 0;
-    int tapTargetCount = 10;
+    int tapTargetCount = 13;
     bool isChecking = false;
     bool canFirstCheck = true;
     bool checkSucceeded = false;
     float getCheckTimer = 0f;
     float getCheckDuration = 2.0f;
-    float giveUpCheckTimer = 0f;
-    float giveUpCheckDuration = 0.5f;
 
+    //以下为相机用变量
+    bool cameraCanBackToInitial = false;
+    float cameraInitialSize;
+    float cameraCurrentSize;
+    float cameraScaleWaitTimer = 0f;
+    float cameraScaleWaitDuration = 1.5f;
 
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        CameraInitialSet();
         //ColliderCheck();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        TryToGetSword();
-    }
-
-
-
-    void TryToGetSword()
-    {
         if (canTryToGetSword)
-        {
-            HintKeyAppear();
             TapKeyCheck();
-        }
     }
-    void HintKeyAppear()
-    {
 
-    }//出现提示图标（未完成）
-
-
-    /// <summary>
-    /// 检测是否在拔剑及其结果
-    /// </summary>
-    void TapKeyCheck()
-    {
-        if (Input.GetKeyDown(KeyCode.J) && canFirstCheck && !checkSucceeded)
-        {
-            isChecking = true;
-            canFirstCheck = false;
-            Debug.Log("首次检查");
-        }//第一次按下J，并且可以进行第一次检查，则可以开始检查条件，并且不可再进行第一次检查
-        if (isChecking)
-        {
-            IsGettingSword();
-            GetTimeCheck();
-            GetCountCheck();
-            //Debug.Log("isChecking is:" + isChecking);
-        }//如果是正在进行检查，那么就调用计时器和计数器
-    }//检测是否拔剑
-    void GetTimeCheck()
-    {
-        //checkTimer = Time.deltaTime;
-        getCheckTimer += Time.deltaTime;
-        if (getCheckTimer >= getCheckDuration)//如果倒计时结束就失败
-        {
-            FailToGetSword();
-        }
-    }//拔剑计时器
-    void GetCountCheck()
-    {
-        //GiveUpGetCheck();
-        Debug.Log("次数检查");
-        if (Input.GetKeyDown(KeyCode.J) && !canFirstCheck)
-        {
-            tapCounter++;
-            //Debug.Log($"{tapCounter}");
-        }
-        if (tapCounter >= tapTargetCount)//如果成功达到了次数
-        {
-            Debug.Log("succeed to get sword");
-            SwordIsGot();
-            canFirstCheck = false;
-            checkSucceeded = true;
-        }
-    }//拔剑计数器
-    void GiveUpGetCheck()
-    {
-        ///
-        ///尚未完成，逻辑有误，还需修正
-        ///
-        giveUpCheckTimer += Time.deltaTime;
-        if (giveUpCheckTimer >= giveUpCheckDuration)//如果倒计时结束就失败
-        {
-            FailToGetSword();
-        }
-    }//拔剑放手计时器（未完成）
-    void IsGettingSword()
-    {
-        animator.SetBool("isGetting", true);
-    }//拔剑中
-    void FailToGetSword()
-    {
-        //那么就退出检查状态，并且可以重新进行第一次检查
-        getCheckTimer = 0;
-        tapCounter = 0;
-        isChecking = false;
-        canFirstCheck = true;
-        animator.SetBool("isGetting", false);
-        Debug.Log("fail to get sword");
-    }//拔剑失败
-    void SwordIsGot()
-    {
-        animator.SetTrigger("got");
-    }//拔剑成功
 
 
     /// <summary>
@@ -142,7 +62,10 @@ public class SwordInStoneBehavior : MonoBehaviour
         if (other.gameObject == Neko)
         {
             canTryToGetSword = true;
-            Debug.Log("canGetSword is: " + canTryToGetSword);
+            JKey.seeable = true;
+            JKey.canSelfRotate = true;
+            JKey.canSelfScale = true;
+            //Debug.Log("canGetSword is: " + canTryToGetSword);
         }
     }//进入
     private void OnTriggerExit2D(Collider2D other)
@@ -150,8 +73,132 @@ public class SwordInStoneBehavior : MonoBehaviour
         if (other.gameObject == Neko)
         {
             canTryToGetSword = false;
-            Debug.Log("canGetSword is:" + canTryToGetSword);
+            JKey.seeable = false;
+            JKey.canSelfRotate = false;
+            JKey.canSelfScale = false;
         }
     }//退出
+
+    /// <summary>
+    /// 检测是否在拔剑及其结果
+    /// </summary>
+    void TapKeyCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && canFirstCheck && !checkSucceeded)
+        {
+            isChecking = true;
+            canFirstCheck = false;
+            //Debug.Log("首次检查");
+        }//第一次按下J，并且可以进行第一次检查，则可以开始检查条件，并且不可再进行第一次检查
+        if (isChecking)
+        {
+            IsGettingSword();
+            GetTimeCheck();
+            GetCountCheck();
+            CameraTryBackToInitial();
+            //Debug.Log("isChecking is:" + isChecking);
+        }//如果是正在进行检查，那么就调用计时器和计数器
+    }//检测是否拔剑
+    void GetTimeCheck()
+    {
+        getCheckTimer += Time.deltaTime;
+        if (getCheckTimer >= getCheckDuration)//如果倒计时结束就失败
+        {
+            FailToGetSword();
+        }
+    }//拔剑计时器
+    void GetCountCheck()
+    {
+        //GiveUpGetCheck();
+        //Debug.Log("次数检查");
+        if (Input.GetKeyDown(KeyCode.J) && !canFirstCheck)
+        {
+            tapCounter++;
+            //GiveUpToGetCheck();
+            //Debug.Log(giveUpCheckTimer);
+        }
+        if (tapCounter >= tapTargetCount)//如果成功达到了次数
+        {
+            SwordIsGot();
+        }
+    }//拔剑计数器
+    void IsGettingSword()
+    {
+        cameraCanBackToInitial=false;
+        animator.SetBool("isGetting", true);
+        CameraTapScale();
+        JKey.canSelfRotate = false;
+        JKey.canSelfScale = false;
+        JKey.isTappingKey = true;
+    }//拔剑中
+    void FailToGetSword()
+    {
+        //那么就退出检查状态，并且重置状态
+        //giveUpCheckTimer = 0;
+        cameraCanBackToInitial = true;
+        getCheckTimer = 0;
+        tapCounter = 0;
+        isChecking = false;
+        animator.SetBool("isGetting", false);
+        //Debug.Log("失败");
+        JKey.canSelfRotate = true;
+        JKey.canSelfScale = true;
+        JKey.isTappingKey = false;
+        StartCoroutine(FailedTimerCoroutine(1.5f));
+    }//拔剑失败
+    IEnumerator FailedTimerCoroutine(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        canFirstCheck = true;
+    }//等待一小会才能再次拔剑
+    void SwordIsGot()
+    {
+        canFirstCheck = false;
+        cameraCanBackToInitial = true;
+        checkSucceeded = true;
+        animator.SetTrigger("got");
+        //Debug.Log("拔出来了");
+        JKey.seeable = false;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }//拔剑成功
+
+
+    /// <summary>
+    /// 各种其他效果
+    /// </summary>
+    void CameraInitialSet()
+    {
+        cameraInitialSize = Camera.orthographicSize;
+        cameraCurrentSize = cameraInitialSize;
+    }//相机数值初始化
+    void CameraTapScale()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            cameraCurrentSize = cameraCurrentSize * 0.97f;
+            Camera.orthographicSize = cameraCurrentSize;
+        }
+    }//按下按键的时候进行一次镜头缩放
+    void CameraTryBackToInitial()
+    {
+        if (cameraCanBackToInitial && checkSucceeded)
+        {
+            cameraScaleWaitTimer += Time.deltaTime;//计时开始
+            if (cameraScaleWaitTimer >= cameraScaleWaitDuration)
+            {
+                Camera.orthographicSize = Mathf.Lerp(Camera.orthographicSize, cameraInitialSize, Time.deltaTime * 2.0f);
+                cameraCurrentSize = cameraInitialSize;
+            }
+            return;
+        }
+        if (cameraCanBackToInitial)
+        {
+            Camera.orthographicSize = Mathf.Lerp(Camera.orthographicSize, cameraInitialSize, Time.deltaTime * 1.0f);
+            cameraCurrentSize = cameraInitialSize;
+            //Debug.Log("调用了");
+        }
+
+
+    }//镜头归位
 
 }
